97fefc5f275daa05dd32977b5a47f94bb6485f59
iAmDaigoOhYeah <gcaoaad@gmail.com>
2021-06-01 23:00:29 +0900

    Vue.js動いた！！

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
